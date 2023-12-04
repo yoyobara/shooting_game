c63817a3755d036c95116ba8d101d440eaa34322
yoyobara <yotambarak6@gmail.com>
2023-12-04 17:09:25 +0200

    basic client sdl2 window

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,7 +1,29 @@
 #[path = "../common.rs"]
 mod common;
 
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
+
 fn main() {
-    common::common_func();
-    println!("THIS IS CLIENT");
+    let sdl_ctx = sdl2::init().unwrap();
+    let video_subsystem = sdl_ctx.video().unwrap();
+
+    let window = video_subsystem.window("Rust Shooter Game", 600, 600).position_centered().build().unwrap();
+    let mut renderer = window.into_canvas().build().unwrap();
+
+    let mut event_pump = sdl_ctx.event_pump().unwrap();
+
+    'mainloop: loop {
+        for e in event_pump.poll_iter() {
+            match e {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'mainloop;
+                }
+                _ => {}
+            }
+        }
+
+        renderer.set_draw_color(Color::GREY);
+        renderer.clear();
+        renderer.present();
+    }
 }
