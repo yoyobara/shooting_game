1da56d8cda9f52833d0e0632507f9a5f4a87a788
yoyobara <yotambarak6@gmail.com>
2023-12-04 19:22:08 +0200

    simple controller example

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,16 +1,23 @@
 #[path = "../common.rs"]
 mod common;
 
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
 
-fn main() {
-    let sdl_ctx = sdl2::init().unwrap();
-    let video_subsystem = sdl_ctx.video().unwrap();
+struct Player {
+    name: String,
+    pos: Rect,
+}
 
-    let window = video_subsystem.window("Rust Shooter Game", 600, 600).position_centered().build().unwrap();
-    let mut renderer = window.into_canvas().build().unwrap();
+fn main() -> Result<(), String> {
+    let sdl_ctx = sdl2::init()?;
+    let video_subsystem = sdl_ctx.video()?;
 
-    let mut event_pump = sdl_ctx.event_pump().unwrap();
+    let window = video_subsystem.window("Rust Shooter Game", 600, 600).position_centered().build().map_err(|e| e.to_string())?;
+    let mut renderer = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    let mut event_pump = sdl_ctx.event_pump()?;
+
+    let joystick = sdl_ctx.joystick()?.open(0).map_err(|e| e.to_string())?;
 
     'mainloop: loop {
         for e in event_pump.poll_iter() {
@@ -22,8 +29,13 @@ fn main() {
             }
         }
 
-        renderer.set_draw_color(Color::GREY);
+        let i = joystick.axis(5).map_err(|e| e.to_string())? / 256;
+        let j = (i + 128) as u8;
+        println!("{}", j);
+        renderer.set_draw_color(Color::RGB(j, j, j));
         renderer.clear();
         renderer.present();
     }
+
+    Ok(())
 }
